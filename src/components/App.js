8c2be33aa0c2564ce3c4f030e9e10266af2452fb
KevinMendieta <kevin.mendieta-p@mail.escuelaing.edu.co>
2018-08-28 21:06:23 -0500

    format form

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
-import TodoList from './TodoList';
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import "../App.css";
+import TodoList from "./TodoList";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
-    this.todos = [{text:"Learn React", priority:5, dueDate: new Date() },
-          {text:"Learn about APIs", priority:4, dueDate: new Date(2018,8,30) },
-          {text:"write TODO App", priority:3, dueDate: new Date(2018,9,30) }];
-    this.state = {items: this.todos, text: ''};
+    this.todos = [
+      { text: "Learn React", priority: 5, dueDate: new Date() },
+      { text: "Learn about APIs", priority: 4, dueDate: new Date(2018, 8, 30) },
+      { text: "write TODO App", priority: 3, dueDate: new Date(2018, 9, 30) }
+    ];
+    this.state = { items: this.todos, text: "" };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -31,7 +32,7 @@ class App extends Component {
     };
     this.setState(prevState => ({
       items: prevState.items.concat(newItem),
-      text: ''
+      text: ""
     }));
   }
 
@@ -44,27 +45,23 @@ class App extends Component {
         </header>
         <div className="App-body">
           <div className="container">
-            <div className="row"> 
-              <TodoList todoList={this.state.items}></TodoList>
+            <div className="row">
+              <TodoList todoList={this.state.items} />
             </div>
-            <div className="row"> 
-              <form onSubmit={this.handleSubmit}>
-                <label htmlFor="new-todo">
-                  What needs to be done?
-                </label>
+            <hr />
+            <div className="row">
+              <form className="form" onSubmit={this.handleSubmit}>
+                <label htmlFor="new-todo">What needs to be done?</label>
                 <input
                   id="new-todo"
                   onChange={this.handleChange}
                   value={this.state.text}
                 />
-                <button>
-                  Add #{this.state.items.length + 1}
-                </button>
-                </form>
-              </div>
+                <button>Add #{this.state.items.length + 1}</button>
+              </form>
             </div>
+          </div>
         </div>
-        
       </div>
     );
   }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Todo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-    }
-    render() {
-        return (
-            <tr>
-                <td>{this.props.text.toString()}</td>
-                <td>{this.props.priority.toString()}</td>
-                <td>{this.props.dueDate.toString()}</td>
-            </tr>            
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <tr>
+        <td>{this.props.text.toString()}</td>
+        <td>{this.props.priority.toString()}</td>
+        <td>{this.props.dueDate.toString()}</td>
+      </tr>
+    );
+  }
 }
 
 export default Todo;
